dec2c1f1c41a40482e84b8d94223577730ff0ca5
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-07-24 19:53:30 -0500

    adding full lib

---
+++ 09.fcfs/src/sRAD_java/gui/sComponent/STextArea.java
@@ -0,0 +1,54 @@
+package sRAD_java.gui.sComponent;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+import static sRAD_java.gui.component.Resource.*;
+import static sRAD_java.gui.component.Theme.taF;
+
+public class STextArea extends JTextArea {
+
+    public final static int SHOW_INFO_TYPE = 0;
+    public final static int GET_INFO_TYPE = 1;
+
+    /**
+     * GET_INFO_TYPE
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     */
+    public STextArea(int x, int y, int width, int height) {
+        setProperties(x, y, width, height, true, true, "", DTII14, DTII1, fontText, DTII1Border, LEFT_ALIGNMENT);
+    }
+
+    /**
+     * SHOW_INFO_TYPE
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param text
+     */
+    public STextArea(int x, int y, int width, int height, String text) {
+        setProperties(x, y, width, height, false, true, text, taF, null, fontText, null, LEFT_ALIGNMENT);
+    }
+
+    public void setProperties(int x, int y, int width, int height, Boolean editable, Boolean lineWrap, String text, Color foreground, Color background,
+                              Font font, Border border, Float hAlignment
+    ) {
+        this.setBounds(x, y, width, height);
+        setText(text);
+        setEditable(editable);
+        setForeground(foreground);
+        setFont(font);
+        setBackground(background);
+        setCaretColor(foreground);
+        setBorder(border);
+        setWrapStyleWord(lineWrap);
+        setLineWrap(lineWrap);
+        setAlignmentX(hAlignment);
+    }
+
+}
